44b138e404abf247a141738913eebf54b2bfe5ed
alice010101 <anphawolf@gmail.com>
2023-12-21 21:13:36 +0700

    updated web

--- src/components/Header.tsx
+++ src/components/Header.tsx
@@ -8,14 +8,18 @@ export const Header: React.FC<React.PropsWithChildren> = ({ children }) => {
       <div>
         <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
           <NavLink className={styles.logo} to="/">
-            <img alt="Gamba logo" src="https://poemonk.com/images/0poem.png" />
-            <span>PeomCasino</span>
+            <img alt="Gamba logo" src="BoneLogoOnly.png" />
+            <span>BONE CASINO</span>
           </NavLink>
         </div>
         <div style={{ display: "flex", gap: "20px", alignItems: "center" }}>
           <div className={styles.externalLinks}>
-            <a href="https://poemonk.com" target="_blank" rel="noreferrer">
-              Poem Website
+            <a
+              href="https://www.bonesolana.xyz/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Bone Website
             </a>
           </div>
           {children}
